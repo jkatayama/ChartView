7576cba57dde161b8ac726ab126a3bd3cef003cb
jumpe.katayama <junktym123@gmail.com>
2023-01-30 09:30:58 +0900

    wip

--- Sources/SwiftUICharts/LineChart/LineChartView.swift
+++ Sources/SwiftUICharts/LineChart/LineChartView.swift
@@ -38,7 +38,7 @@ public struct LineChartView: View {
 //
 //        }
 //    }
-    var frame = CGSize(width: 180, height: 120)
+    var frame: CGSize
 //    private var rateValue: Int?
     
     public init(data: [(Date, Double)],
@@ -74,77 +74,80 @@ public struct LineChartView: View {
     }
     
     public var body: some View {
-        ZStack(alignment: .center){
-            RoundedRectangle(cornerRadius: 0)
-                .fill(self.bgColor)
-                .frame(width: frame.width, height: frame.height, alignment: .center)
-//                .shadow(color: self.style.dropShadowColor, radius: self.dropShadow ? 8 : 0)
-            VStack(alignment: .leading){
-//                if(!self.showIndicatorDot){
-//                    VStack(alignment: .leading, spacing: 8){
-//                        Text(self.title)
-//                            .font(.title)
-//                            .bold()
-//                            .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
-//                        if (self.legend != nil){
-//                            Text(self.legend!)
-//                                .font(.callout)
-//                                .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.legendTextColor :self.style.legendTextColor)
-//                        }
-//                        HStack {
-//
-//                            if let rateValue = self.rateValue
-//                            {
-//                                if (rateValue ?? 0 >= 0){
-//                                    Image(systemName: "arrow.up")
-//                                }else{
-//                                    Image(systemName: "arrow.down")
-//                                }
-//                                Text("\(rateValue)%")
-//                            }
-//                        }
-//                    }
-//                    .transition(.opacity)
-//                    .animation(.easeIn(duration: 0.1))
-//                    .padding([.leading, .top])
-//                }else{
-//                    HStack{
-//                        Spacer()
-//                        Text("\(self.currentValue ?? 0, specifier: self.valueSpecifier)")
-//                            .font(.system(size: 41, weight: .bold, design: .default))
-//                            .offset(x: 0, y: 30)
-//                        Spacer()
-//                    }
-//                    .transition(.scale)
-//                }
-//                Spacer()
-                GeometryReader{ geometry in
-                    Line(data: self.data,
-                         frame: .constant(geometry.frame(in: .local)),
-                         touchLocation: self.$touchLocation,
-                         showIndicator: self.$showIndicatorDot,
-                         minDataValue: .constant(self.data.points.map { $0.1 }.min()),
-                         maxDataValue: .constant(self.data.points.map { $0.1 }.max()),
-                         color: chartColor
-                    )
-                }
-                .frame(width: frame.width, height: frame.height)
-//                .clipShape(RoundedRectangle(cornerRadius: 20))
-                .offset(x: 0, y: 0)
-            }.frame(width: self.formSize.width, height: self.formSize.height)
-        }
-        .gesture(DragGesture()
-        .onChanged({ value in
-            self.touchLocation = value.location
-            self.showIndicatorDot = true
-            self.getClosestDataPoint(toPoint: value.location, width:self.frame.width, height: self.frame.height)
-            self.onValueSelected((Date(), 0))
-        })
-            .onEnded({ value in
-                self.showIndicatorDot = false
-                self.onValueSelected(nil)
+        GeometryReader { geo in
+            ZStack(alignment: .center){
+                RoundedRectangle(cornerRadius: 0)
+                    .fill(self.bgColor)
+                    .frame(width: geo.frame(in: .local).width, height: geo.frame(in: .local).height, alignment: .center)
+    //                .shadow(color: self.style.dropShadowColor, radius: self.dropShadow ? 8 : 0)
+                VStack(alignment: .leading){
+    //                if(!self.showIndicatorDot){
+    //                    VStack(alignment: .leading, spacing: 8){
+    //                        Text(self.title)
+    //                            .font(.title)
+    //                            .bold()
+    //                            .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.textColor : self.style.textColor)
+    //                        if (self.legend != nil){
+    //                            Text(self.legend!)
+    //                                .font(.callout)
+    //                                .foregroundColor(self.colorScheme == .dark ? self.darkModeStyle.legendTextColor :self.style.legendTextColor)
+    //                        }
+    //                        HStack {
+    //
+    //                            if let rateValue = self.rateValue
+    //                            {
+    //                                if (rateValue ?? 0 >= 0){
+    //                                    Image(systemName: "arrow.up")
+    //                                }else{
+    //                                    Image(systemName: "arrow.down")
+    //                                }
+    //                                Text("\(rateValue)%")
+    //                            }
+    //                        }
+    //                    }
+    //                    .transition(.opacity)
+    //                    .animation(.easeIn(duration: 0.1))
+    //                    .padding([.leading, .top])
+    //                }else{
+    //                    HStack{
+    //                        Spacer()
+    //                        Text("\(self.currentValue ?? 0, specifier: self.valueSpecifier)")
+    //                            .font(.system(size: 41, weight: .bold, design: .default))
+    //                            .offset(x: 0, y: 30)
+    //                        Spacer()
+    //                    }
+    //                    .transition(.scale)
+    //                }
+    //                Spacer()
+                    GeometryReader{ geometry in
+                        Line(data: self.data,
+                             frame: .constant(geometry.frame(in: .local)),
+                             touchLocation: self.$touchLocation,
+                             showIndicator: self.$showIndicatorDot,
+                             minDataValue: .constant(self.data.points.map { $0.1 }.min()),
+                             maxDataValue: .constant(self.data.points.map { $0.1 }.max()),
+                             color: chartColor
+                        )
+                    }
+                    .frame(width: geo.frame(in: .local).width, height: geo.frame(in: .local).height)
+    //                .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .offset(x: 0, y: 0)
+                }.frame(width: geo.frame(in: .local).width, height: geo.frame(in: .local).height)
+            }
+            .gesture(DragGesture()
+            .onChanged({ value in
+                self.touchLocation = value.location
+                self.showIndicatorDot = true
+                self.getClosestDataPoint(toPoint: value.location, width:geo.frame(in: .local).width, height: geo.frame(in: .local).height)
+                self.onValueSelected((Date(), 0))
             })
-        )
+                .onEnded({ value in
+                    self.showIndicatorDot = false
+                    self.onValueSelected(nil)
+                })
+            )
+
+        }
     }
     
     @discardableResult func getClosestDataPoint(toPoint: CGPoint, width:CGFloat, height: CGFloat) -> CGPoint {
@@ -164,21 +167,27 @@ public struct LineChartView: View {
 struct WidgetView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            LineChartView(
-                data: [
-                    (Date(),8),
-                    (Date(timeInterval: 60*60*24*7, since: Date()),282),
-                    (Date(timeInterval: 60*60*24*14, since: Date()),502),
-                    (Date(timeInterval: 60*60*24*21, since: Date()),12),
-                    (Date(timeInterval: 60*60*24*28, since: Date()),37),
-                    (Date(timeInterval: 60*60*24*35, since: Date()),7),
-                    (Date(timeInterval: 60*60*24*42, since: Date()),285.019),
-                    (Date(timeInterval: 60*60*24*49, since: Date()),4)
-                ],
-                chartColor: Color.blue, currentValue: .constant(0), selectedValue: .constant(nil), onValueSelected: { _ in
+            GeometryReader { geo in
+                LineChartView(
+                    data: [
+                        (Date(),8),
+                        (Date(timeInterval: 60*60*24*7, since: Date()),282),
+                        (Date(timeInterval: 60*60*24*14, since: Date()),502),
+                        (Date(timeInterval: 60*60*24*21, since: Date()),12),
+                        (Date(timeInterval: 60*60*24*28, since: Date()),37),
+                        (Date(timeInterval: 60*60*24*35, since: Date()),7),
+                        (Date(timeInterval: 60*60*24*42, since: Date()),285.019),
+                        (Date(timeInterval: 60*60*24*49, since: Date()),4)
+                    ],
+                    chartColor: Color.blue,
+                    currentValue: .constant(0),
+                    selectedValue: .constant(nil),
+                    onValueSelected: { _ in
 
-            })
-                .environment(\.colorScheme, .light)
-        }
+                })
+                    .environment(\.colorScheme, .light)
+            }
+
+            }
     }
 }
